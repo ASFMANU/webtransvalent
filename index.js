274bc646ede02dd42f70d98956a4e2d11b6c35a3
ASFMANU <andressaraviaflores@gmail.com>
2022-08-25 13:09:01 -0500

    modifique la barra de menu

--- index.js
+++ index.js
@@ -14,3 +14,21 @@ function servicios(){
 function contactos(){
 	location.href="contactos.html";
 }
+
+/*let headerSup = document.querySelectorAll('#headerSup');*/
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function(){
+	let ubicacion_Actual = window.pageYOffset;
+	if (ubicacionPrincipal<=116){//412
+		document.getElementById('headerSup').style.display = 'block';
+		document.getElementById('imgMenu').style.display = 'none';
+		/*headerSup.style.top = '0';*/
+	}else{
+		/*headerSup.style.top = '-100px';*/
+		document.getElementById('headerSup').style.display = 'none';
+		document.getElementById('imgMenu').style.display = 'block';
+		document.getElementById('imgMenu').style.margin = '15px';
+	}
+	ubicacionPrincipal=ubicacion_Actual;
+
+}
